d780ce0440c5e1344995914d70c7bcb1195ff8a6
Denys Gaspar <53532689+dgaspar1@users.noreply.github.com>
2021-09-22 11:40:15 -0300

    fix: renaming variable names

--- routes/cadastro.js
+++ routes/cadastro.js
@@ -3,24 +3,26 @@ const router = express.Router();
 
 const client = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dhc1l.mongodb.net?retryWrites=true&w=majority`;
+let usuario;
 
 client.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
         if(error) {
             throw error;
         }
         usuario = client.db('Portal-Vacinas').collection('Usuario');
+        console.log(`Entidade Conectada: Usuario`);
     }
 );
 
 const post = function (req, res) {
-    const usuario = new Usuario({
+    const novoUsuario = new Usuario({
         "nome": req.body.nome,
         "sobrenome": req.body.sobrenome,
         "email": req.body.email,
         "senha": req.body.senha
      });
 
-    db.insertOne(usuario, (err, result) => {
+    usuario.insertOne(novoUsuario, (err, result) => {
         if (err) return console.log("Error: " + err);
         console.log('Usuário cadastrado com sucesso!');
         res.send('Usuário cadastrado com sucesso!');
