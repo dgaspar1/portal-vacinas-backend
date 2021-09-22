d5d637e4aafc9881b96e4e3225da3c950e191460
Denys Gaspar <53532689+dgaspar1@users.noreply.github.com>
2021-09-22 11:41:09 -0300

    fix: renaming variable names

--- routes/listaUsuarios.js
+++ routes/listaUsuarios.js
@@ -4,11 +4,14 @@ const router = express.Router();
 const client = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dhc1l.mongodb.net?retryWrites=true&w=majority`;
 
+let usuario;
+
 client.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
         if(error) {
             throw error;
         }
         usuario = client.db('Portal-Vacinas').collection('Usuario');
+        console.log(`Entidade Conectada: Usuario`);
     }
 );
 
