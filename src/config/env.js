d369b2407a3a9da3887edf1b559812b70a5cdc1b
Denys Gaspar <53532689+dgaspar1@users.noreply.github.com>
2021-09-22 16:33:08 -0300

    refactor: finished

---
+++ src/config/env.js
@@ -0,0 +1,7 @@
+require('dotenv/config');
+
+module.exports = {
+  mongoUrl: process.env.MONGO_URL,
+  tokenSecret: process.env.TOKEN_SECRET || 'secret',
+  port: process.env.PORT || 4000
+}
---
+++ src/routes/listaUsuarios-routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const MongoHelper = require('../helpers/mongo-helper');
+
+const get = async function(req, res) {
+    const usuario = await MongoHelper.getCollection('Usuario');
+    
+    usuario.find({}).toArray((err, result) => {
+        if (err) return console.log("Error: " + err);
+            res.send(result);
+        }
+    );
+};
+
+module.exports = router => {
+    router.get('/ListaUsuarios', get);
+};
